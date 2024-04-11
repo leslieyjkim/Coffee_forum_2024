64f3feb61353c1e4e35be343c5fae3570314baf0
yoonjookim <yjblossom@gmail.com>
2024-04-10 22:05:24 -0400

    Switch raw data into 'STATE'.

--- Project_coffee_forum/src/App.jsx
+++ Project_coffee_forum/src/App.jsx
@@ -4,15 +4,20 @@ import ReplyForm from './components/ReplyForm';
 import ReplyList from './components/ReplyList';
 
 //Import point should be the highest in the three that is relevant
-import { forumPost } from "./data/postData";
+import { forumPost as initialPostData } from "./data/postData";
 import { usersObj } from "./data/userData";
 
 
+//Do we need state? Yes.
+//Other than the JSON data from outside, 
+//we have to manipulate and do things with it, then we need 'state'.
 
 function App() {
+  const [forumPost, setForumPost] = useState(initialPostData);
+  const [users, setUsers] = useState(usersObj);
 
   const replies = forumPost.replies
-  const forumPostAuthor = usersObj[forumPost.authorId]; //Add the position of the forumPost/authorId onto usersObj
+  const forumPostAuthor = users[forumPost.authorId]; //Add the position of the forumPost/authorId onto usersObj
   return (
     <>
       <Header />
